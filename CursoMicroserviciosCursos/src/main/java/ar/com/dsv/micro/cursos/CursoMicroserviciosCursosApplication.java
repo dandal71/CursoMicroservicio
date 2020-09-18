aa5488a910eb7d3a4b6586033648c8a352b3427b
Daniel. E. Dalmagro <dandal71@gmail.com>
2020-09-18 20:30:24 -0300

    Inicio

---
+++ CursoMicroserviciosCursos/src/main/java/ar/com/dsv/micro/cursos/CursoMicroserviciosCursosApplication.java
@@ -0,0 +1,13 @@
+package ar.com.dsv.micro.cursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoMicroserviciosCursosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoMicroserviciosCursosApplication.class, args);
+	}
+
+}
---
+++ CursoMicroserviciosCursos/src/test/java/ar/com/dsv/micro/cursos/CursoMicroserviciosCursosApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.dsv.micro.cursos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoMicroserviciosCursosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
